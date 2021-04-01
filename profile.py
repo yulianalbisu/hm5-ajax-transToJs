a245c573f8052410f63732727ce0694324b2a3e4
Yuliana <ya.aldrich@yahoo.com>
2021-04-01 15:37:25 +0000

    comments done

--- profile.py
+++ profile.py
@@ -15,14 +15,14 @@ def index():
 @app.route('/profile', methods=['POST'])
 def profile():
     """Return results from profile form."""
-# TODO: get the values from the rest of the form
+# TODO: get the values from the rest of the formgit
     # Add them to jsonify
 
-    fullname = request.form['name']
-    age = request.form['age']
-    occupation = request.form['occupation']
+    fullname = request.form.get['name'] #get that form from website post request
+    age = request.form.get['age'] #give me the age
+    occupation = request.form.get['occupation'] #give me occupation
     
-    
+    # turn this file into a string, if is its fine just use it
     return jsonify({'fullname': fullname, 'age': age, 'occupation': occupation})
 
 
--- static/profile.js
+++ static/profile.js
@@ -3,15 +3,15 @@ function submitProfile(evt) {
   evt.preventDefault();
 
   const formData = {
-    name: $('#name-field').val(),
-    age: $('#age-field').val(),
-    occupation: $('#occupation-field').val()
+    name: $('#name-field').val(), //id in POST method <label>Name 
+    age: $('#age-field').val(), //id in Age
+    occupation: $('#occupation-field').val() //check the built in functions in Js.
   };
 
-  $.post('/profile', formData, (response) => {
-    $('#profile').append(`
-      <li>${response.fullname} the ${response.occupation} is ${response.age}</li>
-    `);
+  $.post('/profile', formData, (response) => { //in the route /profile in .py go get all the info
+    $('#profile').append(` //when i went to browser, i ask jQuery where's my request form and this is the address in html 
+      <li>${response.fullname} the ${response.occupation} is ${response.age}</li> //response.fullname ==> viene de request.form.get the name in the form 
+    `); //same with the rest, occupation, age, define in profile.py in route /profile
   });
 }
 
